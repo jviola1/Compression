a9e567d258084ce5c4f217dcfcef557952d1614b
jviola1 <jamescviola@gmail.com>
2017-02-28 11:27:23 -0500

    Initial Commit

---
+++ Compression/MainForm.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Compression
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstFiles = new System.Windows.Forms.ListBox();
+            this.btnAddFile = new System.Windows.Forms.Button();
+            this.btnCompress = new System.Windows.Forms.Button();
+            this.txtDirName = new System.Windows.Forms.TextBox();
+            this.btnExtract = new System.Windows.Forms.Button();
+            this.selectFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.compressLocationDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.selectZipDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // lstFiles
+            // 
+            this.lstFiles.FormattingEnabled = true;
+            this.lstFiles.Location = new System.Drawing.Point(12, 12);
+            this.lstFiles.Name = "lstFiles";
+            this.lstFiles.Size = new System.Drawing.Size(172, 290);
+            this.lstFiles.TabIndex = 0;
+            // 
+            // btnAddFile
+            // 
+            this.btnAddFile.Location = new System.Drawing.Point(206, 12);
+            this.btnAddFile.Name = "btnAddFile";
+            this.btnAddFile.Size = new System.Drawing.Size(102, 43);
+            this.btnAddFile.TabIndex = 1;
+            this.btnAddFile.Text = "Add File";
+            this.btnAddFile.UseVisualStyleBackColor = true;
+            this.btnAddFile.Click += new System.EventHandler(this.btnAddFile_Click);
+            // 
+            // btnCompress
+            // 
+            this.btnCompress.Location = new System.Drawing.Point(206, 61);
+            this.btnCompress.Name = "btnCompress";
+            this.btnCompress.Size = new System.Drawing.Size(102, 43);
+            this.btnCompress.TabIndex = 2;
+            this.btnCompress.Text = "Compress";
+            this.btnCompress.UseVisualStyleBackColor = true;
+            this.btnCompress.Click += new System.EventHandler(this.btnCompress_Click);
+            // 
+            // txtDirName
+            // 
+            this.txtDirName.Location = new System.Drawing.Point(206, 124);
+            this.txtDirName.Name = "txtDirName";
+            this.txtDirName.Size = new System.Drawing.Size(102, 20);
+            this.txtDirName.TabIndex = 3;
+            this.txtDirName.Text = "JCVCompression";
+            this.txtDirName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnExtract
+            // 
+            this.btnExtract.Location = new System.Drawing.Point(206, 258);
+            this.btnExtract.Name = "btnExtract";
+            this.btnExtract.Size = new System.Drawing.Size(102, 43);
+            this.btnExtract.TabIndex = 4;
+            this.btnExtract.Text = "Extract";
+            this.btnExtract.UseVisualStyleBackColor = true;
+            this.btnExtract.Click += new System.EventHandler(this.btnExtract_Click);
+            // 
+            // selectZipDialog
+            // 
+            this.selectZipDialog.Filter = "Zip files|*.zip";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(332, 313);
+            this.Controls.Add(this.btnExtract);
+            this.Controls.Add(this.txtDirName);
+            this.Controls.Add(this.btnCompress);
+            this.Controls.Add(this.btnAddFile);
+            this.Controls.Add(this.lstFiles);
+            this.Name = "MainForm";
+            this.Text = "JCVCompression";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstFiles;
+        private System.Windows.Forms.Button btnAddFile;
+        private System.Windows.Forms.Button btnCompress;
+        private System.Windows.Forms.TextBox txtDirName;
+        private System.Windows.Forms.Button btnExtract;
+        private System.Windows.Forms.OpenFileDialog selectFileDialog;
+        private System.Windows.Forms.FolderBrowserDialog compressLocationDialog;
+        private System.Windows.Forms.OpenFileDialog selectZipDialog;
+    }
+}
+
---
+++ Compression/MainForm.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Windows.Forms;
+
+namespace Compression
+{
+    public partial class MainForm : Form
+    {
+        FileCollection fList = new FileCollection();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        //Allows the user to select files on their compujter to be added to the FileCollection
+        private void btnAddFile_Click(object sender, EventArgs e)
+        {
+            selectFileDialog.ShowDialog();
+            if (!(selectFileDialog.FileName.Equals("")))
+                fList.addFile(selectFileDialog.FileName);
+            populateListBox();
+        }
+
+        //Adds elements of the FileCollection to the listbox
+        private void populateListBox()
+        {
+            lstFiles.Items.Clear();
+            for(int i = 0; i < fList.getLength(); i++)
+            {
+                if (!(fList[i] == null))
+                    lstFiles.Items.Add(fList[i]);
+            }
+        }
+
+        //User selects a directory. A new directory is created where all the selected files are copied to
+        //The new directory is then compressed into a .zip file of the same name and the directory is deleted
+        private void btnCompress_Click(object sender, EventArgs e)
+        {
+            compressLocationDialog.SelectedPath = "";
+            compressLocationDialog.ShowDialog();
+
+            if(!(compressLocationDialog.Equals("")))
+            {
+                DirectoryInfo newDir = Directory.CreateDirectory(Path.Combine(compressLocationDialog.SelectedPath, txtDirName.Text));
+
+                for (int x = 0; x < fList.getLength(); x++)
+                {
+                    if (!(fList[x] == null))
+                        System.IO.File.Copy(fList[x], Path.Combine(newDir.FullName, getFileName(fList[x])), true);
+                }
+
+                try
+                {
+                    ZipFile.CreateFromDirectory(newDir.FullName, newDir.FullName + ".zip");
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("The file " + newDir.FullName + ".zip already exists.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+                newDir.Delete(true);
+
+                Process.Start(newDir.FullName + ".zip");
+
+                fList.clear();
+                populateListBox();
+            }
+            
+        }
+
+        //When given a full path, will return just the file name and the extension
+        private string getFileName(string s)
+        {
+            return s.Substring(s.LastIndexOf(@"\") + 1);
+        }
+
+        //User browses their computer to find a .zip file, which will then be extracted into a directory of the same name
+        private void btnExtract_Click(object sender, EventArgs e)
+        {
+            selectZipDialog.FileName = "";
+            selectZipDialog.ShowDialog();
+            string zName = selectZipDialog.FileName;
+
+            if (!(zName.Equals("")))
+            {
+                DirectoryInfo exDir = Directory.CreateDirectory(zName.Substring(0, zName.LastIndexOf(@".")));
+
+                try
+                {
+                    ZipFile.ExtractToDirectory(zName, exDir.FullName);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("The directory " + exDir.FullName + " already exists.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+                Process.Start(exDir.FullName);
+            }
+        }
+    }
+
+    //Used to keep track of selected files
+    public class FileCollection
+    {
+        string[] fileList = new string[10];
+
+        public string this[int i]
+        {
+            get
+            {
+                return fileList[i];
+            }
+
+            set
+            {
+                fileList[i] = value;
+            }
+        }
+
+        //Adds a file to the first empty slot in the array
+        public void addFile(string tFile)
+        {
+            for(int x = 0; x < fileList.Length; x++)
+            {
+                if(fileList[x] == null)
+                {
+                    fileList[x] = tFile;
+                    break;
+                }
+            }
+        }
+
+        public int getLength()
+        {
+            return fileList.Length;
+        }
+
+        //Clears the entire array
+        public void clear()
+        {
+            for (int x = 0; x < fileList.Length; x++)
+                fileList[x] = null;
+        }
+    }
+}
